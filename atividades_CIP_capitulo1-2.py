00ea8e16e18975e278a2b6bd7a973f86ad6182e8
Maria Clara F. Alves <ferreiraalvesmariaclara3@gmail.com>
2026-07-27 11:21:33 -0300

    Respostas das atividades do livro Curso Intensivo de Python, capitulo 1 e 2

---
+++ atividades_CIP_capitulo1-2.py
@@ -0,0 +1,81 @@
+print('==========================================')
+print('2.1 – simple_message')
+print('==========================================')
+
+mensagem = "Olá! Estou aprendendo Python."
+print(mensagem)
+
+mensagem = "Agora estou praticando exercícios de Python. \n"
+print(mensagem)
+
+
+print('==========================================')
+print('2.3 – personal_message')
+print('==========================================')
+
+nome = input("Digite seu nome: ")
+print(f"Olá {nome}, você gostaria de aprender um pouco de Python hoje? \n")
+
+
+print('==========================================')
+print('2.4 – name_cases')
+print('==========================================')
+
+nome = input("Digite um nome:")
+
+print("Minúsculas:", nome.lower())
+print("Maiúsculas:", nome.upper())
+print("Primeira letra maiúscula:", nome.title())
+
+
+print('\n ==========================================')
+print('2.5 – famous_quote.py')
+print('==========================================')
+
+print('Marco Aurélio certa vez disse: "Você tem poder sobre sua mente, não sobre os acontecimentos externos.\n"')
+
+
+print('==========================================')
+print ('2.6 – famous_quote_2')
+print('==========================================')
+
+famous_person = "Marco Aurélio"
+
+message = f'{famous_person} certa vez disse: "Você tem poder sobre sua mente, não sobre os acontecimentos externos."'
+
+print(message)
+
+
+print('==========================================')
+print('2.7 – stripping_names.py')
+print('==========================================')
+
+nome = "\t\n João Pedro \n\t"
+
+print(nome)
+print(nome.lstrip())
+print(nome.rstrip())
+print(nome.strip())
+
+
+print('\n ==========================================')
+print('2.8 – number_eight')
+print('==========================================')
+
+print(5 + 3)
+print(10 - 2)
+print(4 * 2)
+print(16 // 2)
+
+
+print('\n ==========================================')
+print('2.9 – favorite_number')
+print('==========================================')
+# Autor: Maria Clara F. Alves
+# Este programa mostra meu número favorito.
+numero_favorito = 7
+
+print(f"Meu número favorito é {numero_favorito}. \n")
+
+
+
